fb587d4ada4c44304ce11107cffb37eb8e881fce
vale <valemil.vs@gmail.com>
2020-07-09 15:35:33 +0200

    Fine con soluzione dadi

--- js/myscript.js
+++ js/myscript.js
@@ -1,24 +1,53 @@
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
+// // Chiedi all’utente la sua email,
+// // controlla che sia nella lista di chi può accedere,
+// // stampa un messaggio appropriato sull’esito del controllo.
+//
+// var mail = prompt ("Inserisci la tua mail");
+//
+// var mailList = ["abc@boolean.it" , "def@boolean.it" , "ghi@boolean.it" , "lmn@boolean.it" , "opq@boolean.it" , "rst@boolean.it" , "uvz@boolean.it" , "studenteClasse16@boolean.it"];
+// console.log([mailList]);
+//
+// var trovata = false;
+//
+// for (var i = 0; i < mailList.length; i++){
+//
+//     if (mail == mailList[i]) {
+//     trovata = true;
+//     }
+//
+// }
+//
+// if (trovata == true) {
+//     alert("Controllo effettuato con successo! Puoi accedere al sito");
+// } else {(
+//     alert("Mail non trovata. Impossibile accedere al sito");
+// }
 
-var mail = prompt ("Inserisci la tua mail");
 
-var mailList = ["abc@boolean.it" , "def@boolean.it" , "ghi@boolean.it" , "lmn@boolean.it" , "opq@boolean.it" , "rst@boolean.it" , "uvz@boolean.it" , "studenteClasse16@boolean.it"];
-console.log([mailList]);
+// ***Gioco dei dadi***
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-var trovata = false;
+var giocatoreRandom = Math.floor(Math.random()*6) + 1;
+console.log(giocatoreRandom);
 
-for (var i = 0; i < mailList.length; i++){
+var pcRandom = Math.floor(Math.random()*6) + 1;
+console.log(pcRandom);
 
-    if (mail == mailList[i]) {
-    trovata = true;
-    }
+var vincitore;
+var pareggio;
+
+if (giocatoreRandom > pcRandom) {
+    vincitore = giocatoreRandom;
+    document.getElementById('numero-vincitore').innerHTML = "il numero con il punteggio più alto è " + vincitore;
 
 }
+else if (pcRandom > giocatoreRandom) {
+    vincitore = pcRandom;
+    document.getElementById('numero-vincitore').innerHTML = "il numero con il punteggio più alto è " + vincitore;
 
-if (trovata == true) {
-    alert("Controllo effettuato con successo! Puoi accedere al sito");
-} else {
-    alert("Mail non trovata. Impossibile accedere al sito");
+}
+else {
+    pareggio = giocatoreRandom == pcRandom;
+    document.getElementById('numero-uguale').innerHTML = "i numeri sono uguali";
 }
